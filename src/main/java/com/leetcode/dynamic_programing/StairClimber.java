4d5273b4708badece835e50450174a39d88cebae
Anuj Kumar <cdac.anuj@gmail.com>
2025-07-11 23:08:13 +0200

    improved solution (#2)

--- src/main/java/com/leetcode/dynamic_programing/StairClimber.java
+++ src/main/java/com/leetcode/dynamic_programing/StairClimber.java
@@ -15,4 +15,17 @@ public class StairClimber {
 
         return Math.min(dp[N - 1], dp[N - 2]);
     }
+
+    public int minCostClimbingStairsSpaceImproved(int[] cost) {
+        int n = cost.length;
+        if (n == 1) return cost[0];
+
+        int prev2 = cost[0], prev1 = Math.min(cost[0] + cost[1], cost[1]);
+        for (int i = 2; i < n; i++) {
+            int curr = Math.min(prev1, prev2) + cost[i];
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return Math.min(prev1, prev2);
+    }
 }
